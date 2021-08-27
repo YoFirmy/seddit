46652144f3837990e87009b1c12939c95fca437a
Matt Firmston <mattfirmston@gmail.com>
2021-08-27 22:33:04 +0100

    added user sign up with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # created as part of devise set up
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
