143a5e3e54c188ead3136c5f127026ab6c52ffb0
Matt Firmston <mattfirmston@gmail.com>
2021-08-28 08:57:39 +0100

    validates message exists in model

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.message = "Here's a message"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a message" do
+    post = Post.new(message: nil)
+    expect(post).to_not be_valid
+  end
 end
