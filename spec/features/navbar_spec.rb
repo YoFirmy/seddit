273b393ea5d2c7806280c096e2f9d9ab523a1b56
Matt Firmston <mattfirmston@gmail.com>
2021-08-29 22:45:42 +0100

    added new post link to nav and usability styling

--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Navbar", type: :feature do
   context "User is not logged in" do
-    scenario "Log out button does not display" do
+    scenario "Log out link does not display" do
       visit "/"
-      expect(page).to have_no_button('Log Out')
+      expect(page).to have_no_link('Log Out')
     end
 
-    scenario "Log-in and Sign-up buttons do display" do
+    scenario "Log-in and Sign-up links do display" do
       visit "/"
-      expect(page).to have_button('Log In')
-      expect(page).to have_button('Sign Up')
+      expect(page).to have_link('Log In')
+      expect(page).to have_link('Sign Up')
     end
   end
 
@@ -19,13 +19,13 @@ RSpec.feature "Navbar", type: :feature do
       sign_up
     end
 
-    scenario "Log out button displays" do
-      expect(page).to have_button('Log Out')
+    scenario "Log out link displays" do
+      expect(page).to have_link('Log Out')
     end
 
-    scenario "Log-in and Sign-up buttons do not display" do
-      expect(page).to have_no_button('Log In')
-      expect(page).to have_no_button('Sign Up')
+    scenario "Log-in and Sign-up links do not display" do
+      expect(page).to have_no_link('Log In')
+      expect(page).to have_no_link('Sign Up')
     end
   end
 end
